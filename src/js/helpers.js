ae58a77f16fe2221094713eedfe3fc67c9a8fec1
Kyle Sutton <31548831+SuttonKyle@users.noreply.github.com>
2025-03-07 11:37:24 -0500

    Update helpers.js

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -6,11 +6,11 @@ Pulsar.registerFunction(
   function (token, tokenGroup, prefix) {
     // Create array with all path segments and token name at the end
     const segments = [...tokenGroup.path];
-    const segmentsRoot = segments.length > 0 ? segments.shift() : "";
-    let usePrefix = prefix && prefix.length > 0;
     if (!tokenGroup.isRoot) {
       segments.push(tokenGroup.name);
     }
+    const segmentsRoot = segments.length > 0 ? segments.shift() : "";
+    let usePrefix = prefix && prefix.length > 0;
     segments.push(token.name);
     switch(segmentsRoot) {
       case "core-color":
